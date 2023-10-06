b11009cb7bd0ebd5aa8e1bcac799e90905695fee
Ivan Pavlov <ivnpvl@disroot.org>
2023-10-06 20:59:48 +0300

    Add many-to-many to admin, docker first try

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,12 +69,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'django'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', ''),
+            'PORT': os.getenv('DB_PORT', 5432),
+        }
+    }
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -11,16 +11,37 @@ class IngredientAdmin(ModelAdmin):
 
 @register(Tag)
 class TagAdmin(ModelAdmin):
-    list_display = ('id', 'name', 'slug', 'color')
+    list_display = ('name', 'slug', 'color')
     list_filter = ('name',)
     search_fields = ('name',)
 
 
 @register(Recipe)
 class RecipeAdmin(ModelAdmin):
-    list_display = ('id', 'author', 'name', 'pub_date', 'in_favorite')
+    list_display = (
+        'name',
+        'author',
+        'pub_date',
+        'get_tags',
+        'get_ingredients',
+        'in_favorite',
+    )
     list_filter = ('pub_date', 'author', 'name', 'tags')
     search_fields = ('name',)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related('tags', 'ingredients')
+
+    @display(description='Тэги')
+    def get_tags(self, recipe):
+        return ", ".join([str(tag) for tag in recipe.tags.all()])
+
+    @display(description='Ингредиенты')
+    def get_ingredients(self, recipe):
+        return ", ".join(
+            [str(ingredient) for ingredient in recipe.ingredients.all()]
+        )
 
     @display(description='Число добалений в избранное')
     def in_favorite(self, obj):
