70a473ef3c79141bbf5a23406c68b304c2d7bf6c
Ivan Pavlov <ivnpvl@disroot.org>
2023-10-12 01:45:19 +0300

    Fix paginator and new readme

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = getenv('DJANGO_SECRET_KEY', 'django_secret_key')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS', '*').split()
 
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
 
